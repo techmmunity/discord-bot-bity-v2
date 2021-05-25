06f47d6d29b2c72759fe20a938fff564b4af3d7e
henriqueleite42 <henriqueleite616@gmail.com>
2021-05-25 16:20:00 +0000

    Deploying to release from @ techmmunity/discord-bot-bity-v2@25f4781a9b0e68fba48860e1e9d5d6df498ddf42 🚀

--- index.js
+++ index.js
@@ -668,18 +668,34 @@ Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.sendServerExplanationMessage = void 0;
 const channels_1 = __nccwpck_require__(4206);
 const guilds_1 = __nccwpck_require__(26912);
+const getMessages = ({ message, unlockChannelsId, challenglesId, }) => {
+    if (message.mentions.users.size > 1) {
+        return [
+            "Falem jovens, sejam bem vindos(as)!",
+            `Depois deem uma passada no <#${unlockChannelsId}>`,
+            "Lá vcs podem desbloquear canais pra postar suas duvidas / conteudos",
+            `Nós tbm temos desafios diarios, ao meio dia, no <#${challenglesId}>`,
+        ];
+    }
+    return [
+        "Fala jovem, seja bem vindo(a)!",
+        `Depois dá uma passada no <#${unlockChannelsId}>`,
+        "Lá vc pode desbloquear canais pra postar suas duvidas / conteudos",
+        `Nós tbm temos desafios diarios, ao meio dia, no <#${challenglesId}>`,
+    ];
+};
 const sendServerExplanationMessage = async (message) => {
     var _a, _b;
     const guildId = (_a = message.guild) === null || _a === void 0 ? void 0 : _a.id;
     const unlockChannelsId = channels_1.ChannelEnum[guildId].UNLOCK_CHANNELS;
     const challenglesId = channels_1.ChannelEnum[guildId].CHALLENGES;
     const welcomeMessageChannel = (_b = message.guild) === null || _b === void 0 ? void 0 : _b.channels.cache.get(channels_1.ChannelEnum[guildId].WELCOME_MESSAGE);
-    await welcomeMessageChannel.send([
-        "Fala jovens, sejam bem vindos(as)!",
-        `Depois deem uma passada no <#${unlockChannelsId}>`,
-        "Lá vcs podem desbloquear canais pra postar suas duvidas / conteudos",
-        `Nós tbm temos desafios diarios, ao meio dia, no <#${challenglesId}>`,
-    ].join("\n"));
+    const messages = getMessages({
+        message,
+        unlockChannelsId,
+        challenglesId,
+    });
+    await (welcomeMessageChannel === null || welcomeMessageChannel === void 0 ? void 0 : welcomeMessageChannel.send(messages.join("\n")));
 };
 exports.sendServerExplanationMessage = sendServerExplanationMessage;
 //# sourceMappingURL=send-server-explanation-message.js.map
