25f4781a9b0e68fba48860e1e9d5d6df498ddf42
Henrique Leite <henriqueleite616@gmail.com>
2021-05-25 13:18:31 -0300

    Change Welcome Message (#20)

--- src/api/commands/moderation/service/register/helpers/send-server-explanation-message.ts
+++ src/api/commands/moderation/service/register/helpers/send-server-explanation-message.ts
@@ -3,6 +3,34 @@ import { Message, TextChannel } from "discord.js";
 import { ChannelEnum } from "enums/channels";
 import { GuildEnum } from "enums/guilds";
 
+interface GetMessagesParams {
+	message: Message;
+	unlockChannelsId: string;
+	challenglesId: string;
+}
+
+const getMessages = ({
+	message,
+	unlockChannelsId,
+	challenglesId,
+}: GetMessagesParams) => {
+	if (message.mentions.users.size > 1) {
+		return [
+			"Falem jovens, sejam bem vindos(as)!",
+			`Depois deem uma passada no <#${unlockChannelsId}>`,
+			"Lá vcs podem desbloquear canais pra postar suas duvidas / conteudos",
+			`Nós tbm temos desafios diarios, ao meio dia, no <#${challenglesId}>`,
+		];
+	}
+
+	return [
+		"Fala jovem, seja bem vindo(a)!",
+		`Depois dá uma passada no <#${unlockChannelsId}>`,
+		"Lá vc pode desbloquear canais pra postar suas duvidas / conteudos",
+		`Nós tbm temos desafios diarios, ao meio dia, no <#${challenglesId}>`,
+	];
+};
+
 export const sendServerExplanationMessage = async (message: Message) => {
 	const guildId = message.guild?.id as GuildEnum;
 
@@ -13,12 +41,11 @@ export const sendServerExplanationMessage = async (message: Message) => {
 		ChannelEnum[guildId].WELCOME_MESSAGE,
 	) as TextChannel;
 
-	await welcomeMessageChannel.send(
-		[
-			"Fala jovens, sejam bem vindos(as)!",
-			`Depois deem uma passada no <#${unlockChannelsId}>`,
-			"Lá vcs podem desbloquear canais pra postar suas duvidas / conteudos",
-			`Nós tbm temos desafios diarios, ao meio dia, no <#${challenglesId}>`,
-		].join("\n"),
-	);
+	const messages = getMessages({
+		message,
+		unlockChannelsId,
+		challenglesId,
+	});
+
+	await welcomeMessageChannel?.send(messages.join("\n"));
 };
