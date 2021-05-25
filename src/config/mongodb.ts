3dff2d6e2aa009cca58a5d1248b813308a73eae7
Henrique Leite <henriqueleite616@gmail.com>
2021-05-25 18:37:26 -0300

    Fix Bot Database Connection (#22)

--- src/config/mongodb.ts
+++ src/config/mongodb.ts
@@ -2,10 +2,12 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 
 const { NODE_ENV, MONGODB_URL } = process.env;
 
+const notIsPrd = NODE_ENV !== "production";
+
 export const MongoDbConnect = TypeOrmModule.forRoot({
 	type: "mongodb",
 	url: MONGODB_URL,
 	synchronize: false,
-	logging: NODE_ENV !== "production",
-	entities: ["dist/entities/*.entity.js"],
+	logging: notIsPrd,
+	entities: [notIsPrd ? "dist/entities/*.entity.js" : "entities/*.entity.js"],
 });
