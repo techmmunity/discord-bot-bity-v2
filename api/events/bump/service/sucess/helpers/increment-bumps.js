c8fd974a99d967df8947766796340e49bc420075
henriqueleite42 <henriqueleite616@gmail.com>
2021-05-26 11:46:03 +0000

    Deploying to release from @ techmmunity/discord-bot-bity-v2@d91f2e59c64b0f0dc520aa3610cf14c12e8ea38c 🚀

--- api/events/bump/service/sucess/helpers/increment-bumps.js
+++ api/events/bump/service/sucess/helpers/increment-bumps.js
@@ -3,19 +3,23 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.incrementBumps = void 0;
 const bump_entity_1 = require("../../../../../../entities/bump.entity");
 const incrementBumps = async (BumpRepository, message) => {
+    var _a;
+    const userId = (_a = message.mentions.users.first()) === null || _a === void 0 ? void 0 : _a.id;
+    if (!userId)
+        return 0;
     const userBump = await BumpRepository.findOne({
-        discordUserId: message.author.id,
+        discordUserId: userId,
     });
     if (userBump) {
         await BumpRepository.update({
-            discordUserId: message.author.id,
+            discordUserId: userId,
         }, {
             bumps: userBump.bumps + 1,
         });
         return userBump.bumps + 1;
     }
     await BumpRepository.save({
-        discordUserId: message.author.id,
+        discordUserId: userId,
         bumps: 1,
     });
     return 1;
