e869ccbc965021562bf4f72569ef4f348128d176
Henrique Leite <henriqueleite616@gmail.com>
2021-06-23 10:31:02 -0300

    Fix Bot Mention

--- src/api/events/mention/service/bot/index.ts
+++ src/api/events/mention/service/bot/index.ts
@@ -11,11 +11,16 @@ const isProduction = NODE_ENV === "production";
 const getBotId = () => (isProduction ? BotsEnum.BITY : BotsEnum.BITY_TEST);
 
 export const bot = (message: Message) => {
+	if (message.reference) return;
+
 	const botId = getBotId();
 
-	const user = message?.mentions.users?.get(botId);
+	const user = message.mentions.users?.get(botId);
 
 	if (!user) return;
 
-	return sendEmbed(message);
+	return sendEmbed({
+		message,
+		user,
+	});
 };
