d8989d20a4bef8212c2bd23c6ccc10a88f0287f6
Henrique Leite <henriqueleite616@gmail.com>
2021-08-04 20:50:47 -0300

    Remove BumpRush Role Requirement For Bump

--- src/api/events/bump/service/sucess/index.ts
+++ src/api/events/bump/service/sucess/index.ts
@@ -1,20 +1,8 @@
-import { GuildMember, Message } from "discord.js";
+import { Message } from "discord.js";
 import { BumpRepository } from "entities/bump.entity";
 
 import { incrementBumps } from "./helpers/increment-bumps";
-import { sendDoesntHasRoleEmbed } from "./helpers/send-doest-have-role-embed";
 import { sendSuccessEmbed } from "./helpers/send-sucess-embed";
-
-import { GuildEnum } from "enums/guilds";
-import { RolesEnum } from "enums/roles";
-
-const getBumpRole = (member: GuildMember) => {
-	const guildId = member.guild.id as GuildEnum;
-
-	const bumpRole = member.roles.cache.get(RolesEnum[guildId].BUMP);
-
-	return bumpRole;
-};
 
 export const success = async (
 	bumpRepository: BumpRepository,
@@ -24,31 +12,21 @@ export const success = async (
 
 	if (!member) return;
 
-	const bumpRole = getBumpRole(member);
-
 	const promises: Array<Promise<any>> = [];
 
 	const deleteBotMessagePromise = message.delete();
 
 	promises.push(deleteBotMessagePromise);
 
-	if (bumpRole) {
-		const userBumpsTotal = await incrementBumps(bumpRepository, message);
+	const userBumpsTotal = await incrementBumps(bumpRepository, message);
 
-		const succesMessagePromise = sendSuccessEmbed({
-			message,
-			member,
-			userBumpsTotal,
-		});
+	const successMessagePromise = sendSuccessEmbed({
+		message,
+		member,
+		userBumpsTotal,
+	});
 
-		promises.push(succesMessagePromise);
-	} else {
-		const doesntHasRolePromise = sendDoesntHasRoleEmbed({
-			message,
-		});
-
-		promises.push(doesntHasRolePromise);
-	}
+	promises.push(successMessagePromise);
 
 	return Promise.all(promises);
 };
