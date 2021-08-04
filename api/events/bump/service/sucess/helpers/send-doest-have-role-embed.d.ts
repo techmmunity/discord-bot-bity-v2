e44d2295f2449a80b4edc2480692930e9e307953
henriqueleite42 <henriqueleite616@gmail.com>
2021-08-04 23:59:46 +0000

    Deploying to release from @ techmmunity/discord-bot-bity-v2@7d77cd6b4a9de769112675ea9871a0fba8fe46f9 🚀

--- api/events/bump/service/sucess/helpers/send-doest-have-role-embed.d.ts
+++
@@ -1,6 +0,0 @@
-import { Message } from "discord.js";
-interface SendDoesntHasRoleEmbedParams {
-    message: Message;
-}
-export declare const sendDoesntHasRoleEmbed: ({ message, }: SendDoesntHasRoleEmbedParams) => Promise<Message>;
-export {};
