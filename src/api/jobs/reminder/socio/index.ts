f9a0b5213f3c8be13a95f97358ae630666bb7cb7
Henrique Leite <henriqueleite616@gmail.com>
2021-08-06 17:07:41 -0600

    Fix Reminders (#40)

--- src/api/jobs/reminder/socio/index.ts
+++ src/api/jobs/reminder/socio/index.ts
@@ -21,19 +21,15 @@ export const sendSocioReminder = async (
 
 	Pra quem não sabe, a Tech tem o objetivo de ser uma **empresa** de educação e criação de ferramentas que auxiliam no dia a dia das empresas.
 
-	Eu por enquanto estou levando esse barco "sozinho" (temos uma equipe foda que ajuda em muita coisa || Vlw Alessandro e Ju, é nóis :love: ||), mas precisava de outra pessoa full time nisso, com sangue nos olhos pra fazer esse ngc dar certo.
+	Eu por enquanto estou levando esse barco sozinho, mas precisava de outra pessoa full time nisso, com sangue nos olhos pra fazer acontecer.
 
 	Eu sei como é foda vender teu sonho pra outra pessoa, mas acho q em dupla daria pra ter muito mais resultado e bem mais rapido do que sozinho.
 
 	Eu to procurando alguém que tenha principois parecidos e habilidades complementarem (frase feita de "Primo Rico", mas não deixa de ser verdade):
-	- Meu objetivo com a tech é fazer com que ela tenha uma qualidade absurda, já que acho que isso é um dos pilares que sustentam uma empresa (Só q no começo vai sair tudo "cagado" mesmo, já que não temos pessoal suficiente e precisamos fazer as coisas **rapido**).
-	- O que eu tenho pra oferecer pra empresa é a habilidade tecnica e o capital financeiro, e eu preciso de uma pessoa que seja **OU** muito boa com pessoas, carismatica, etc, **OU** que seja um otimo dev.
+	- Meu objetivo com a tech é fazer com que ela tenha sempre foco em **qualidade**, já que acho que isso é um dos pilares que sustentam uma empresa (Só q no começo vai sair tudo "cagado" mesmo, já que não temos pessoal suficiente e precisamos fazer as coisas **rapido**).
+	- Eu quero alguém que realmente queira dar a alma por isso mano, sartup no Brasil é um inferno, e precisa ter muita dedicação e vontade pra fazer isso dar certo
+	- Eu tenho uma experiencia bacana com startup e contato com uma galera que realmente manja muito de negocios, o que falta mesmo é mais braços pra fazer a tech acontecer.
+	- De preferência, a pessoa faria um papel mais de "CEO", cuidando da parte "externa" da empresa, conversando com socios e vendendo a ideia pra investidores, além de cuidar de uma parte mais de marketing no começo, esse tipo de coisa, enquanto eu posso focar nos produtos e fazer uma coisa FODA.
 
-	Observações importantes que acho que possam despertar mais do seu interesse:
-	- Eu já fali duas empresas, então confia que eu tenho experiencia no que **NÃO** fazer :joy:
-	- Temos um "consultor", que tem uma empresa multimilionária, e tá ajudando muito com conselhos e acompanhando o desenvolvimento da Tech.
-
-	Se você tiver interesse e achar que é qualificado, seja pelas coisas que eu citei ou por alguma outra skill sua, manda uma DM pra mim (<@${UsersEnum.RAZAL}>) e bora trocar uma ideia respeito, eu posso te apresentar mais sobre a Tech, sobre o que já temos construido e o que planejo pro futuro.
-
-	PS: Por motivos obvios, sim, você precisa ser maior de 18.`);
+	Se você tiver interesse e achar que é qualificado, seja pelas coisas que eu citei ou por alguma outra skill sua, manda uma DM pra mim (<@${UsersEnum.RAZAL}>) e bora trocar uma ideia a respeito, eu posso te apresentar mais sobre a Tech, sobre o que já temos construido e o que planejo pro futuro.`);
 };
